db626e993b60e409bf56f884fa7c01b4759dede7
Sojo C Johny <sojocjohny37@gmail.com>
2022-07-16 22:38:09 +0530

    Added Header for user and admin & some basic card layouts

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,33 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let products=[
+    {
+      name:"Iphone 12",
+      category:"mobile",
+      description:"6 GB RAM, 128 GB Storage, Full HD+ Retina Display, 2.5GHz 6 core A15 Bionic Processor",
+      image:"https://www.trustedreviews.com/wp-content/uploads/sites/54/2022/03/Samsung-Galaxy-S22-Plus-home-screen-4-scaled.jpg"
+    },
+    {
+      name:"Samsung Galaxy S22 Ultra",
+      category:"mobile",
+      description:"6 GB RAM, 128 GB Storage, Full HD+ Retina Display, 2.5GHz 8 core Snapdragon 888+ Processor",
+      image:"https://www.trustedreviews.com/wp-content/uploads/sites/54/2022/03/Samsung-Galaxy-S22-Plus-home-screen-4-scaled.jpg"
+    },
+    {
+      name:"Xioami 12 Pro",
+      category:"mobile",
+      description:"6 GB RAM, 128 GB Storage, Full HD+ Retina Display, 2.5GHz 8 core Snapdragon 888+ Processor",
+      image:"https://www.trustedreviews.com/wp-content/uploads/sites/54/2022/03/Samsung-Galaxy-S22-Plus-home-screen-4-scaled.jpg"
+    },
+    {
+      name:"IQOO 9 Legend",
+      category:"mobile",
+      description:"6 GB RAM, 128 GB Storage, Full HD+ Retina Display, 8 core Snapdragon 888+ Processor",
+      image:"https://www.trustedreviews.com/wp-content/uploads/sites/54/2022/03/Samsung-Galaxy-S22-Plus-home-screen-4-scaled.jpg"
+    }
+  ]
+  res.render('index', { products, admin:true });
 });
 
 module.exports = router;
