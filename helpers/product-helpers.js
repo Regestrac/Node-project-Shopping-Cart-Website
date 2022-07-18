150a58b8e91f8a3c4fa452d0636f9db8a09033fc
Sojo C Johny <sojocjohny37@gmail.com>
2022-07-19 00:13:51 +0530

    Created Signup and Login page with basic authentication

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,5 +1,5 @@
 var db= require('../config/connection');
-var collection = require('../config/collections')
+var collection = require('../config/collections');
 module.exports = {
     addProduct:(product,callback) => {
         console.log(product);
